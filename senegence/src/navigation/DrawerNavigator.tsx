0cf8439b2a0d3e32f10c5d012a19fcc3601f9194
ayyappap3232 <ayyappa.p@photon.in>
2021-10-01 10:28:38 +0530

    initial route name changed

--- senegence/src/navigation/DrawerNavigator.tsx
+++ senegence/src/navigation/DrawerNavigator.tsx
@@ -69,7 +69,7 @@ const DrawerNavigator = () => {
   return (
     <Drawer.Navigator
       screenOptions={{drawerType: 'front', drawerStyle: {width: 293}}}
-      initialRouteName={ScreenNames.MainShoppingCartBag}
+      initialRouteName={ScreenNames.StartUpDrawer}
       drawerContent={props => (
         <CustomDrawerContent {...props} categoryData={categoryData} />
       )}>
