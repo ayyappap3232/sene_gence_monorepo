6370c8adf6034d1876d64a95dacfca0d49d8cc72
ayyappap3232 <ayyappa.p@photon.in>
2021-10-22 12:42:25 +0530

    Mapping the title to appropriate urls in the all products screen

--- senegence/src/utils/data/AllProducts.tsx
+++ senegence/src/utils/data/AllProducts.tsx
@@ -4,46 +4,64 @@ export const allProducts = [
     {
         id: 'alp1',
         image: images.lips,
-        title: 'Lips'
+        title: 'Lips',
+        name: 'Lip',
+        url_path: "shop/cosmetics/lip"
     },
     {
         id: 'alp2',
         image: images.face,
-        title: 'Face'
+        title: 'Face',
+        name: "Face",
+        url_path: "shop/cosmetics/face"
     },
     {
         id: 'alp3',
         image: images.eyes,
-        title: 'Eyes'
+        title: 'Eyes',
+        name: "Eye",
+        url_path: "shop/cosmetics/eye"
     },
     {
         id: 'alp4',
         image: images.body,
-        title: 'Body'
+        title: 'Body',
+        name: "BATH & BODY",
+        url_path: "shop/bath-body"
     },
     {
         id: 'alp5',
         image: images.men,
-        title: 'Men'
+        title: 'Men',
+        name: "MENS",
+        url_path: "shop/mens"
     },
     {
         id: 'alp6',
         image: images.haircare,
-        title: 'Hair Care'
+        title: 'Hair Care',
+        name: "HAIR",
+        url_path: "shop/hair"
     },
     {
         id: 'alp7',
         image: images.allcollections,
-        title: 'All Collections'
+        title: 'All Collections',
+        name: "SHOP & SAVE",
+        url_path: "shop"
     },
     {
         id: 'alp8',
         image: images.skincare,
-        title: 'Skin Care'
+        title: 'Skin Care',
+        name: "SKINCARE",
+        url_path: "shop/skincare"
     },
     {
         id: 'alp9',
         image: images.cosmetics,
-        title: 'Cosmetics'
+        title: 'Cosmetics',
+        name: "COSMETICS",
+        url_path: "shop/cosmetics"
     }
 ]
